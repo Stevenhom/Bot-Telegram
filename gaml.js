9dcd35f9a5e85930326c75710c24967d751f9c71
Steven Razafimahery <stevenmahery@gmail.com>
2025-05-29 15:22:08 +0300

    Correction galm.js

--- gaml.js
+++ gaml.js
@@ -46,12 +46,11 @@ async function login() {
             nodeEnv: process.env.NODE_ENV,
             chromeSource: process.env.PUPPETEER_EXECUTABLE_PATH || 'Puppeteer intégré'
         });
-        console.log(`✅ Puppeteer utilise ce navigateur : ${await browser.version()}`);
 
 
         console.log(`Options de lancement: ${JSON.stringify(launchOptions, null, 2)}`);
         browser = await puppeteerExtra.launch(launchOptions);
-        
+        console.log(`✅ Puppeteer utilise ce navigateur : ${await browser.version()}`);
         console.log(`[${((Date.now() - startTime) / 1000).toFixed(3)}s] Navigateur lancé`);
 
         page = await browser.newPage();
